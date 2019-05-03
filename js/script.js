98706f71e488fc1446e87b3d8ec39c373c718b10
E-McNeill <ericaksaunders@gmail.com>
2019-05-03 10:08:37 -0700

    final edits

--- js/script.js
+++ js/script.js
@@ -9,7 +9,6 @@ fetch('https://randomuser.me/api/?results=12&nat=us&inc=picture,name,email,locat
 /*Build Search Container*/
 $('.search-container').append('<form action="#" method="get"></form>');
 $('form').append('<input type="search" id="search-input" class="search-input" placeholder="Search...">');
-$('#search-input').after('<input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">');
 
 /*Search Functionality*/
     $('#search-input').on('keyup', function() {
